9793946b899ed6abda7ce25220130664149af0bb
azulviki <azulesvictoria@gmail.com>
2025-11-02 15:35:36 -0300

    Conectar form a Formspree

--- js/contacto.js
+++ js/contacto.js
@@ -10,15 +10,36 @@ createApp({
     };
   },
   methods: {
-    enviarFormulario() {
+    async enviarFormulario() {
       if (this.nombre && this.email && this.mensaje) {
-        this.exito = true;
-        // limpiamos el formulario
-        this.nombre = '';
-        this.email = '';
-        this.mensaje = '';
-        // ocultar mensaje luego de unos segundos
-        setTimeout(() => this.exito = false, 3000);
+      try {
+        const response = await fetch("https://formspree.io/f/xkgpyogo", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          nombre: this.nombre,
+          email: this.email,
+          mensaje: this.mensaje
+        })
+      });
+
+
+          if (response.ok) {
+            this.exito = true;
+            this.nombre = '';
+            this.email = '';
+            this.mensaje = '';
+            setTimeout(() => this.exito = false, 3000);
+          } else {
+            alert("Hubo un problema al enviar el mensaje : (");
+          }
+        } catch (error) {
+          console.error("Error:", error);
+          alert("Hubo un problema al enviar el mensaje : (");
+        }
       }
     }
   }
